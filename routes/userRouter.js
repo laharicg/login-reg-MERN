818b0c412694127b84610e496a634fead01645df
lahari govada <govada.lahari@gmail.com>
2020-09-03 20:50:23 +0530

    jwt

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 // router.get("/test", (req, res) => {
 //   res.send("Hello! it is working");
 // });
 
 router.post("/register", async (req, res) => {
   try {
-    const { email, password, passwordCheck, displayName } = req.body;
+    let { email, password, passwordCheck, displayName } = req.body;
 
     //validate
 
@@ -37,6 +38,34 @@ router.post("/register", async (req, res) => {
     });
     const savedUser = await newUser.save();
     res.json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //validate
+    if (!email || !password)
+      return res.status(400).json({ msg: "Enter required fields" });
+
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials!" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+        email: user.email,
+      },
+    });
   } catch (err) {
     res.status(500).json(err);
   }
